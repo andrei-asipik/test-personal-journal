aabb16f95b0ed4c310291a128489a4ef750bed90
Andrei Asipik <osipik.av@yandex.by>
2024-05-16 22:16:13 +0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,37 @@
+import './App.css';
+// import Button from './assets/components/Button/Button';
+// import JournalItem from './assets/components/JournalItem/JournalItem';
+
+function App() {
+  // const data = [
+  //   {
+  //     title: 'Title 1',
+  //     text: 'Text 1',
+  //     date: new Date()
+  //   },
+  //   {
+  //     title: 'Title 2',
+  //     text: 'Text 2',
+  //     date: new Date()
+  //   }
+  // ];
+
+  return (
+    <div>
+      <h2>Project</h2>
+      {/* <Button />
+      <JournalItem
+        title={data[0].title}
+        text={data[0].text}
+        date={data[0].date}
+      />
+      <JournalItem
+        title={data[1].title}
+        text={data[1].text}
+        date={data[1].date}
+      /> */}
+    </div>
+  );
+}
+
+export default App;

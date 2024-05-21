a9ed935564ad5988f5e0d318a41546dbb4f43dbe
Andrei Asipik <osipik.av@yandex.by>
2024-05-22 01:43:47 +0300

    added left pannel components

--- src/components/JournalAddButton/JournalAddButton.jsx
+++ src/components/JournalAddButton/JournalAddButton.jsx
@@ -2,7 +2,12 @@ import CardButton from '../CardButton/CardButton';
 import './JournalAddButton.css';
 
 function JournalAddButton() {
-  return <CardButton className="journal-add">Новое воспоминание</CardButton>;
+  return (
+    <CardButton className="journal-add">
+      <img src="./plus.svg" alt="plus" />
+      Новое воспоминание
+    </CardButton>
+  );
 }
 
 export default JournalAddButton;
